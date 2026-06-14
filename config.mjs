f60f8abd20c49dc8eba3c5c475805219c6f361dd
Daniel Schep <dschep@gmail.com>
2026-06-13 21:13:54 -0400

    ultra build!

---
+++ config.mjs
@@ -0,0 +1,9 @@
+// Ultra config, primarily to enable persisting viewport
+export const defaultMode = "map";
+export const modes = {
+  map: {
+    settings: {
+      persistState: true,
+    },
+  },
+};
